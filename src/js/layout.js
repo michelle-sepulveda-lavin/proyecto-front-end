d0f0375b67aab0b52438f87aa613acabbb179336
German Mora <german.mora.664@gmail.com>
2020-09-15 14:23:20 -0300

    Vista de planes

--- src/js/layout.js
+++ src/js/layout.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import injectContext from "./store/appContext";
 import Home from './views/home'
+import Planes from './views/planes';
 
-const Layout = () =>{
-    return(
+const Layout = () => {
+    return (
         <>
             <BrowserRouter>
                 <Switch>
                     <Route exact path='/' component={Home} />
+                    <Route exact path='/planes' component={Planes} />
                 </Switch>
             </BrowserRouter>
         </>
