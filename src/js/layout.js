7bb6bea6da00e8f859bf3a78aa4610788087d490
Paul Guzmán <andres22.pgpa@gmail.com>
2020-09-15 14:19:09 -0300

    home-footer

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import injectContext from "./store/appContext";
-import Home from './views/home'
+import Home from './views/home';
+import Footer from './views/footer';
+import MenuNavegacion from './views/superAdmin/menuNavegacion'
 
-const Layout = () =>{
-    return(
+const Layout = () => {
+    return (
         <>
             <BrowserRouter>
                 <Switch>
+                    {/* <Route exact path='/' component={Login} />
+                    <Route exact path='/' component={Planes} /> */}
                     <Route exact path='/' component={Home} />
+                    <Route render={() => <h1 className="notfound">Not found!</h1>} />
                 </Switch>
+                <Footer />
             </BrowserRouter>
         </>
     )
