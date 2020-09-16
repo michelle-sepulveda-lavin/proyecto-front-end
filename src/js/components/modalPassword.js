83e7bcf0c0f93d8f19cbca8529b78fe39a6d5e38
michelle-sepulveda-lavin <mic.sepulveda@outlook.com>
2020-09-16 20:42:13 -0300

    fetch recuperacion contraseña, mejoramiento alerta crearEdificios

--- src/js/components/modalPassword.js
+++ src/js/components/modalPassword.js
@@ -1,15 +1,12 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { Context } from '../store/appContext';
 
 const ModalPassword = () => {
+    const {store, actions} = useContext(Context)
     const [correo, setCorreo] = useState(null);
-    const [test, setTest] = useState(null);
 
     const savePassword = (e) => {
         setCorreo(e.target.value)
-    };
-    const handleSubmit = e => {
-        e.preventDefault();
-        setTest(correo);
     };
 
     return (
@@ -19,22 +16,19 @@ const ModalPassword = () => {
                     <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title m-auto pl-5" id="staticBackdropLabel">Recuperar Contraseña</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
                         </div>
                         {
-                            !!test ?
+                            !!store.msgEmail ?
                                 <>
                                     <div className="modal-body">
-                                        <p>Se ha enviado un enlace a tu correo, Revisalo!</p>
+                                        <p>{store.msgEmail}</p>
                                     </div>
                                     <div className="modal-footer">
-                                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                        <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={actions.resetMsg}>Cerrar</button>
                                     </div>
                                 </>
                                 :
-                                <form onSubmit={e => { handleSubmit(e) }}>
+                                <form onSubmit={e => { actions.buscarEmail(e, correo) }}>
                                     <div className="form-group">
                                         <div className="modal-body">
                                             <label htmlFor="password">Email</label>
--- src/js/views/crearEdificios.js
+++ src/js/views/crearEdificios.js
@@ -2,14 +2,8 @@ import React, { useContext, useState } from 'react';
 import { Context } from '../store/appContext';
 
 const CrearEdificios = () => {
-    const {store, actions} = useContext(Context)
+    const { store, actions } = useContext(Context)
     const [state, setState] = useState({})
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        actions.crearEdificio(state)
-        e.target.reset()
-    };
 
     const handleChange = (e) => {
         setState({ ...state, [e.target.id]: e.target.value })
@@ -21,67 +15,77 @@ const CrearEdificios = () => {
                 <div className="container">
                     <h2 className="d-flex justify-content-center">Formulario inicializacion edificio</h2>
                 </div>
+                {
+                    store.error !== null &&
+                    <div className="alert alert-warning alert-dismissible fade show" role="alert">
+                        <strong>Error</strong>{" "}
+                        {store.error}
+                        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                }
                 <div className="container mt-4 py-5">
-                    <form className="col-12 col-md-6 m-auto border py-5" onSubmit={e => { handleSubmit(e) }}>
+                    <form className="col-12 col-md-6 m-auto border py-5" onSubmit={e => { actions.crearEdificio(e, state) }}>
                         {/* <div className="form-group">
                             <label htmlFor="archivoCSV">Archivo CSV</label>
                             <input className="form-control-file" type="file" id="archivoCSV" />
                         </div> */}
                         <div className="form-group">
                             <label htmlFor="nombre_edificio">Nombre edificio</label>
-                            <input type="text" className="form-control" id="nombre_edificio" onChange={e => { handleChange(e) }} required />
+                            <input type="text" className="form-control" id="nombre_edificio" onChange={e => { handleChange(e) }} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="direccion">Direccion</label>
-                            <input type="text" className="form-control" id="direccion" onChange={e => { handleChange(e) }} required />
+                            <input type="text" className="form-control" id="direccion" onChange={e => { handleChange(e) }} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="nombre_administrador">Nombre administrador</label>
-                            <input type="text" className="form-control" id="nombre_administrador" onChange={e => { handleChange(e) }} required />
+                            <input type="text" className="form-control" id="nombre_administrador" onChange={e => { handleChange(e) }}  />
                         </div>
                         <div className="form-group">
                             <label htmlFor="telefono">Telefono </label>
-                            <input type="tel" className="form-control" id="telefono" onChange={e => { handleChange(e) }} required />
+                            <input type="tel" className="form-control" id="telefono" onChange={e => { handleChange(e) }}  />
                         </div>
                         <div className="form-group">
                             <label htmlFor="correo">Correo</label>
-                            <input type="email" className="form-control" id="correo" onChange={e => { handleChange(e) }} required />
+                            <input type="email" className="form-control" id="correo" onChange={e => { handleChange(e) }}  />
                         </div>
                         <div className="form-group">
                             <label htmlFor="numero_pisos">Pisos totales</label>
-                            <input type="number" className="form-control" id="numero_pisos" onChange={e => { handleChange(e) }} required />
+                            <input type="number" className="form-control" id="numero_pisos" onChange={e => { handleChange(e) }}  />
                         </div>
                         <div className="form-group">
                             <label htmlFor="numero_departamentos">Cantidad departamentos</label>
-                            <input type="number" className="form-control" id="numero_departamentos" onChange={e => { handleChange(e) }} required />
+                            <input type="number" className="form-control" id="numero_departamentos" onChange={e => { handleChange(e) }}  />
                         </div>
                         <div className="form-group">
                             <label htmlFor="total_bodegas">Bodegas totales</label>
-                            <input type="number" className="form-control" id="total_bodegas" onChange={e => { handleChange(e) }} required />
+                            <input type="number" className="form-control" id="total_bodegas" onChange={e => { handleChange(e) }}  />
                         </div>
                         <div className="form-group">
                             <label htmlFor="total_estacionamientos">Estacionamientos totales</label>
-                            <input type="number" className="form-control" id="total_estacionamientos" onChange={e => { handleChange(e) }} required />
+                            <input type="number" className="form-control" id="total_estacionamientos" onChange={e => { handleChange(e) }}  />
                         </div>
                         <div className="form-group">
                             <label htmlFor="inicio_contratacion">Fecha inicio contrato</label>
-                            <input type="date" className="form-control" id="inicio_contratacion" onChange={e => { handleChange(e) }} required />
+                            <input type="date" className="form-control" id="inicio_contratacion" onChange={e => { handleChange(e) }}  />
                         </div>
                         <div className="form-group">
                             <label htmlFor="termino_contrato">Fecha termino contrato</label>
-                            <input type="date" className="form-control" id="termino_contrato" onChange={e => { handleChange(e) }} required />
+                            <input type="date" className="form-control" id="termino_contrato" onChange={e => { handleChange(e) }}  />
                         </div>
                         <div className="form-group">
                             <label htmlFor="plan_id">Plan Id</label>
-                            <input type="number" className="form-control" id="plan_id" onChange={e => { handleChange(e) }} required />
+                            <input type="number" className="form-control" id="plan_id" onChange={e => { handleChange(e) }}  />
                         </div>
                         <div className="form-group">
                             <label htmlFor="username_id">Usuario Id</label>
-                            <input type="number" className="form-control" id="username_id" onChange={e => { handleChange(e) }} required />
+                            <input type="number" className="form-control" id="username_id" onChange={e => { handleChange(e) }}  />
                         </div>
                         <div className="form-group">
                             <label htmlFor="dia_vencimiento">Dia vencimiento gastos comunes</label>
-                            <input type="number" className="form-control" id="dia_vencimiento" onChange={e => { handleChange(e) }} required />
+                            <input type="number" className="form-control" id="dia_vencimiento" onChange={e => { handleChange(e) }}  />
                         </div>
                         <button className="btn btn-primary">Crear edificio</button>
                     </form>
