1d354df2448109964ec8cba4774d7a75234f646c
Paul Guzmán <andres22.pgpa@gmail.com>
2020-09-17 11:46:09 -0300

    17-09

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,20 +5,20 @@ import Home from './views/home'
 import Login from './views/login';
 import Planes from './views/planes';
 import Footer from './components/footer';
-import NavegacionAdm from './components/navegacionAdm';
 import DashboardAdm from './views/dashboardAdm'
 import ListadoEdificioAdm from './views/listadoEdificioAdm'
 import ModifyPlans from './views/planes-SA';
 import CrearEdificios from './views/crearEdificios';
+
 
 const Layout = () => {
     return (
         <>
             <BrowserRouter>
                 <Switch>
-                <Route exact path='/listadoEdificioAdm' component={ListadoEdificioAdm} />
+                    
+                    <Route exact path='/listadoEdificioAdm' component={ListadoEdificioAdm} />
                     <Route exact path='/dashboardAdm' component={DashboardAdm} />
-                    <Route exact path='/navegacionAdm' component={NavegacionAdm} />
                     <Route exact path='/crearedificio' component={CrearEdificios} />
                     <Route exact path='/login' component={Login} />
                     <Route exact path='/' component={Home} />
---
+++ src/js/views/detalleEdificio.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ListadoEdificioAdm = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="/">Logo</a>
+            </nav>
+
+            <div className="container">
+
+            </div>
+
+
+
+        </>
+    )
+};
+
+export default ListadoEdificioAdm;
